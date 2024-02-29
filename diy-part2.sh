c65605f931e6f39290d4bd0dcfdd4408ea8634c4
sdttttt <42728902+sdttttt@users.noreply.github.com>
2024-02-29 11:08:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 获取 immortalwrt 的 DAE 
+cp -rf immortalwrt_pkg/net/dae feeds/packages/net/dae
