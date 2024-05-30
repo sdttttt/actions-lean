3d618c6f32ab29057f1513f0bd12897680105abb
sdttttt <42728902+sdttttt@users.noreply.github.com>
2024-05-30 17:01:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,8 +17,9 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# 使用 O3 级别的优化
-sed -i 's/Os/O3/g' include/target.mk
+#sed -i 's/Os/O3/g' include/target.mk
+# 使用 O2 级别的优化
+sed -i 's/Os/O2/g' include/target.mk
 
 sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefile
 # sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
