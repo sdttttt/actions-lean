4d3703a15fb9af9ad559ed395c84e85adcf6076d
sdttttt <42728902+sdttttt@users.noreply.github.com>
2025-02-28 08:48:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,8 +20,12 @@
 # 使用 O2 级别的优化
 sed -i 's/Os/O3/g' include/target.mk
 
-sed -i 's/# CONFIG_PREEMPT_RT is not set/CONFIG_PREEMPT_RT=y/g' target/linux/generic/config-6.12
-sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' target/linux/x86/Makefile
+# 开启抢占式
+sed -i 's/# CONFIG_PREEMPT is not set/CONFIG_PREEMPT=y/g' target/linux/generic/config-6.6
+# 关闭不可抢占
+sed -i 's/CONFIG_PREEMPT_NONE=y/# CONFIG_PREEMPT_NONE is not set/g' target/linux/generic/config-6.6
+
+# sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' target/linux/x86/Makefile
 # sed -i 's/KERNEL_TESTING_PATCHVER:=6.6/KERNEL_TESTING_PATCHVER:=6.1/g' target/linux/x86/Makefile
 
 # 交换 LAN/WAN 口
