7d27fcea78823efb004de5fd529f0e37fa8558a8
sdttttt <42728902+sdttttt@users.noreply.github.com>
2025-03-12 14:52:58 +0800

    Create optimize-system.sh

---
+++ files/root/optimize-system.sh
@@ -0,0 +1,65 @@
+
+    # 优化系统配置
+
+    if grep -q "1000000" "/etc/profile"; then
+        echo
+        green " 系统网络配置 已经优化过, 不需要再次优化 "
+        echo
+        sysctl -p
+        echo
+        exit
+    fi
+
+    if [ -z $1 ]; then
+        removeOptimizingSystemConfig
+    fi
+
+
+
+    echo
+    green " 开始准备 优化系统网络配置 "
+
+    cat >> /etc/sysctl.conf <<-EOF
+
+fs.file-max = 1000000
+fs.inotify.max_user_instances = 8192
+
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.ip_local_port_range = 1024 65000
+net.ipv4.tcp_max_syn_backlog = 16384
+net.ipv4.tcp_max_tw_buckets = 6000
+net.ipv4.route.gc_timeout = 100
+
+net.ipv4.tcp_syn_retries = 1
+net.ipv4.tcp_synack_retries = 1
+net.core.somaxconn = 32768
+net.core.netdev_max_backlog = 32768
+net.ipv4.tcp_timestamps = 0
+net.ipv4.tcp_max_orphans = 32768
+
+# forward ipv4
+#net.ipv4.ip_forward = 1
+
+
+EOF
+
+
+
+    cat >> /etc/security/limits.conf <<-EOF
+*               soft    nofile          1000000
+*               hard    nofile          1000000
+EOF
+
+
+	echo "ulimit -SHn 1000000" >> /etc/profile
+    source /etc/profile
+
+
+    echo
+	sysctl -p
+
+    echo
+    green " 已完成 系统网络配置的优化 "
+    echo
